63baab168c08896b0cf3c7fb2d28e2043eeaa692
Marcus Ahnve <marcus@ahnve.com>
2018-10-01 12:00:38 +0200

    latest conf

--- terraform/digitalocean.tf
+++ terraform/digitalocean.tf
@@ -6,15 +6,15 @@ provider "digitalocean" {
 }
 
 resource "digitalocean_ssh_key" "default" {
-  name = "demo-tf-key"
+  name = "mahnve-demo-tf-key"
   public_key = "${file("${var.public_key}")}"
 }
 
 resource "digitalocean_droplet" "test_server" {
-  name="demo-test-server"
+  name="mahnve-demo-test-server"
   region="ams2"
   size="512mb"
-  image="debian-8-x64"
+  image="29364322"
   ssh_keys= ["${digitalocean_ssh_key.default.fingerprint}"]
   private_networking=true
 
